3aa94e83d84835a64501c79805984b299de61a9e
Jakub Matusiak <kontakt@jakub-matusiak.pl>
2019-08-17 23:16:14 +0200

    Implement hamburger menu

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+  navigation.classList.toggle('navigation--active');
+});
+
+navigation.addEventListener('click', (e) => {
+  if (e.target.classList.contains('navigation__link')) {
+    navigation.classList.toggle('navigation--active');
+  }
+});
