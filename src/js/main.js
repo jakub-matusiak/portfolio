60b2e716794f737b6d79dd7be96ef64122e53ac9
Jakub Matusiak <kontakt@jakub-matusiak.pl>
2019-08-25 20:51:30 +0200

    Add animation to hamburger button

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@ const navigation = document.querySelector('.navigation');
 
 hamburger.addEventListener('click', () => {
   navigation.classList.toggle('navigation--active');
+  hamburger.classList.toggle('hamburger--active');
 });
 
 navigation.addEventListener('click', (e) => {
   if (e.target.classList.contains('navigation__link')) {
     navigation.classList.toggle('navigation--active');
+    hamburger.classList.toggle('hamburger--active');
   }
 });
