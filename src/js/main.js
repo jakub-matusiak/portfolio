c9c8f8855f69c09538c7acf92ee5b59a6c8935e4
Jakub Matusiak <kontakt@jakub-matusiak.pl>
2019-08-29 22:22:12 +0200

    Add go to top button

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
+const about = document.querySelector('.section--about');
+const top = document.querySelector('.top');
 
 hamburger.addEventListener('click', () => {
   navigation.classList.toggle('navigation--active');
@@ -10,5 +12,14 @@ navigation.addEventListener('click', (e) => {
   if (e.target.classList.contains('navigation__link')) {
     navigation.classList.toggle('navigation--active');
     hamburger.classList.toggle('hamburger--active');
+    top.classList.add('top--active');
+  }
+});
+
+window.addEventListener('scroll', () => {
+  if (about.getBoundingClientRect().top < 50) {
+    top.classList.add('top--active');
+  } else {
+    top.classList.remove('top--active');
   }
 });
